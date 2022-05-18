82891351a4a03228f78d3f5f116f1b60f6198f24
ptrainingexperts@gmail.com <ptrainingexperts@gmail.com>
2022-05-18 18:27:25 +0530

    fix travis

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Link to="/">Home</Link>
-        <Link to="/otherpage">Other Page</Link>
+        <Link to="/otherpage">Other Page.</Link>
       </header>
       <div>
         <Route exact path="/" component={Fib}/>
